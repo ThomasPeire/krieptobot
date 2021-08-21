cea53a96c24c517d7ef14c62f4bb3197f16cbcfb
Thomas Peire <thomas.peire@outlook.com>
2021-08-21 06:57:04 +0200

    added console app launcher

---
+++ Krieptobod.ConsoleLauncher/Startup.Application.cs
@@ -0,0 +1,22 @@
+﻿using KrieptoBod.Application;
+using KrieptoBod.Application.Recommendators;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace KrieptoBod.ConsoleLauncher
+{
+    public static class ApplicationServiceCollectionExtension
+    {
+        public static IServiceCollection AddApplicationStartup(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddScoped<ITrader, Trader>();
+
+            services.AddScoped<IRecommendator, RecommendatorRsi>();
+            services.AddScoped<IRecommendator, RecommendatorSupport>();
+
+            services.AddScoped<IRecommendationCalculator, RecommendationCalculator>();
+
+            return services;
+        }
+    }
+}
---
+++ Krieptobod.ConsoleLauncher/Startup.cs
@@ -0,0 +1,23 @@
+﻿using KrieptoBod.Infrastructure.Bitvavo.Extensions.Microsoft.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace KrieptoBod.ConsoleLauncher
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddApplicationStartup(Configuration);
+
+            services.AddBitvavoService(Configuration);
+        }
+    }
+}
